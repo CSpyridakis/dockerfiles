2acfe6fe6246cbce197f20c94d59b265adbd1abc
Spyridakis Christos <spyridakischristos@gmail.com>
2024-12-28 15:54:33 +0200

    Homepage updates

--- homepage/start.sh
+++ homepage/start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-UID=${UID} GID=${GID} docker compose up -d
+if command -v docker &> /dev/null
+    UID=${UID} GID=${GID} docker compose up -d
+elif command -v podman &> /dev/null
+    UID=${UID} GID=${GID} podman compose up -d
+else
+    echo "A known container runtime is not available"
+fi
