b3e8898875c3e8bb4888b6bb19dc09644ba3ef17
Spyridakis Christos <spyridakischristos@gmail.com>
2025-01-05 13:47:02 +0200

    Move homepage to its own repository

--- homepage/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if command -v docker &> /dev/null
-    UID=${UID} GID=${GID} docker compose up -d
-elif command -v podman &> /dev/null
-    UID=${UID} GID=${GID} podman compose up -d
-else
-    echo "A known container runtime is not available"
-fi
